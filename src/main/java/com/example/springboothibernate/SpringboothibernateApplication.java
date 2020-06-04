561b5a9b91375d732090154607e0205a8d61d7fe
lunylein <lunylein@gmail.com>
2020-06-04 22:53:29 +0200

    initial commit

---
+++ src/main/java/com/example/springboothibernate/SpringboothibernateApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboothibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringboothibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothibernateApplication.class, args);
+    }
+
+}
